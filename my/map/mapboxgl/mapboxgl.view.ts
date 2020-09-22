6f9a97b64dfb1e97d7f0f7a27c344746b1da3038
Nikolai Lebedev <nlebedev@gispro.ru>
2020-09-22 09:03:42 +0300

    rerender issue

--- my/map/mapboxgl/mapboxgl.view.ts
+++ my/map/mapboxgl/mapboxgl.view.ts
@@ -2,18 +2,48 @@ namespace $.$$ {
 
 	export class $my_map_mapboxgl extends $.$my_map_mapboxgl {
 
-		static api() {
+		static fetchResoures() {
+			$mol_import.style( `https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css`)
 			return $mol_import.script( `https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js`).mapboxgl
 		}
 
-		@ $mol_mem
-		api(next? : any , force? : $mol_mem_force) : any {
-
-			const mapboxgl = $my_map_mapboxgl.api()
-			
+		/*
+		constructor(
+			public style : ()=> void ,
+			public center : ()=> void ,
+			public zoom : ()=> void ,
+			public antialias : ()=> void ,
+		) {
+			super()
+			$mol_import.style( `https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css`)
+			const mapboxgl = $mol_import.script( `https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js`).mapboxgl
 			mapboxgl.accessToken = this.accessToken()
 			
 			const api = new mapboxgl.Map( {
+				container: this.dom_node().id,
+				style: this.style,
+				center : this.center,
+				zoom : this.zoom,
+				antialias : this.antialias,
+			} )
+
+			api.on('moveend', ( event : any )=> {
+				this.update( event )
+			} )
+
+			return api
+		}
+		 */
+		
+		
+		@ $mol_mem
+		map(next? : any , force? : $mol_mem_force) : any {
+			
+			const mapboxgl = $my_map_mapboxgl.fetchResoures()
+			
+			mapboxgl.accessToken = this.accessToken()
+			
+			const map = new mapboxgl.Map( {
 				container: this.dom_node().id,
 				style: this.style(),
 				center : this.center(),
@@ -21,7 +51,7 @@ namespace $.$$ {
 				antialias : this.antialias(),
 			} )
 
-			api.on('moveend', ( event : any )=> {
+			map.on('moveend', ( event : any )=> {
 				this.update( event )
 				
 				// new $mol_after_frame( $mol_fiber_root( ()=> {
@@ -30,17 +60,19 @@ namespace $.$$ {
 				
 			} )
 			
-			return api
+			return map
 		}
 		
+		
 		update( event? : any ) {
-			const mapCenter = this.api().getCenter().toArray() 
+			const mapCenter = this.map().getCenter().toArray() 
 			console.log(mapCenter)
-			this.center(mapCenter) // Не работает
+			this.center(mapCenter) // Вызывает render!
 		}
 		
 		render() {
-			const api = this.api()
+			console.log('render!')
+			const map = this.map() // Каждый раз добавляет DOM элементы
 			this.dom_node_actual()
 		}
 
